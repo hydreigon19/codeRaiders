a8935f2b848b252221aca61ca942252def10958b
George <georgecoll97@gmail.com>
2019-10-01 12:27:05 -0500

    Shooting
    shooting

--- codeRaiders game/Assets/Scripts/Animation script/BasicMovement.cs
+++ codeRaiders game/Assets/Scripts/Animation script/BasicMovement.cs
@@ -6,19 +6,34 @@ public class BasicMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
-    private Rigidbody2D rb;
+    public GameObject crosshair;
+    public GameObject bulletPrefab;
+    public bool endOfAiming;
+
+    public float Crosshair_Distance = 1.0f;
+    public float BULLET_BASE_SPEED = 1.0f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    public Animator animator;
+    
     void Update()
     {
 
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
+        
+
         rb.AddForce(movement * speed);
+
+        MoveCrossHair();
+        Shoot();
+
         if (movement != Vector3.zero)
         {
             animator.SetFloat("Horizontal", movement.x);
@@ -28,10 +43,39 @@ public class BasicMovement : MonoBehaviour
         animator.SetFloat("Magnitude", movement.magnitude);
         transform.position = transform.position + movement * Time.deltaTime;
 
+        endOfAiming = Input.GetButtonUp("Fire1");
+
         // transform.position = transform.position + vertical * Time.deltaTime;
     }
     void FixedUpdate()//move our character
     {
 
     }
+
+    void MoveCrossHair(){
+        Vector2 aim = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if(aim != Vector2.zero){
+            if(aim.magnitude > 0.0f){
+                aim.Normalize();
+                crosshair.transform.localPosition = aim * Crosshair_Distance;
+
+            }
+            //crosshair.transform.localPosition = aim * Crosshair_Distance;
+        }
+    }
+    
+    void Shoot(){
+        Vector2 shootingDirection = crosshair.transform.localPosition;
+        shootingDirection.Normalize();
+
+        if(endOfAiming){
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = shootingDirection * BULLET_BASE_SPEED;
+            bullet.transform.Rotate(0,0, Mathf.Atan2(shootingDirection.y, shootingDirection.x)* Mathf.Rad2Deg);
+            Destroy(bullet, 2.0f);
+        }
+
+    }
+    
 }
