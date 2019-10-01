03c734b325f6d529b755bcdcd695b389dc826bd7
chrismoran95 <47925970+chrismoran95@users.noreply.github.com>
2019-10-01 14:01:39 -0500

    Enemy script/shootingblend

--- codeRaiders game/Assets/AI.cs
+++ codeRaiders game/Assets/AI.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class AI : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
