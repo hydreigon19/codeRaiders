0f5fd01838c7a512c7c101db24e8ccffdc6e7355
George <georgecoll97@gmail.com>
2019-10-01 23:41:17 -0500

    camera/timer

--- codeRaiders game/Assets/Scripts/countdownTimer.cs
+++ codeRaiders game/Assets/Scripts/countdownTimer.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class countdownTimer : MonoBehaviour
 {
-    float currentTime;
-    float startingTime;
+    float currentTime = 0f;
+    float startingTime = 180f;
+
+    [SerializeField] Text countdownText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = startingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if(currentTime <= 0 ){
+            currentTime = 0;
+        }
     }
 }
