2114f0c65983be95ff51f83929edbad7765f57a3
George <georgecoll97@gmail.com>
2019-11-05 19:30:06 -0600

    After 1st C-Rank Dual

--- codeRaiders game/Assets/Scripts/Score.cs
+++ codeRaiders game/Assets/Scripts/Score.cs
@@ -17,12 +17,22 @@ public class Score : MonoBehaviour
     void Update()
     {
         scoreText.text = "Score: " + scoreAmount;
-        if(scoreAmount == 10){
+        if(scoreAmount == 12){
             Switch();
+        }
+        else if(scoreAmount >= 6)
+        {
+            if(Input.GetKeyDown(KeyCode.E)){
+                Switch2();
+            }
         }
     }
     public void Switch()
     {
         SceneManager.LoadScene("level2");
+    }
+    public void Switch2()
+    {
+        SceneManager.LoadScene("level3");
     }
 }
